52785a3763592589600e4740c0b2f04304e0219a
JustinVillas <92616184+JustinVillas@users.noreply.github.com>
2023-08-11 15:47:42 +0800

    Add files via upload

---
+++ mainjavascript/script.js
@@ -0,0 +1,82 @@
+// Menu toggle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function (e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+// Typing animation
+var TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+TxtType.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+
+};
+
+// Active Link State on Srcoll
+// Get All Links
+let navLinks = document.querySelectorAll('nav ul li a')
+// Get All Sectiion
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function () {
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if (scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (section.getAttribute('id') == link.getAttribute('href').substring(1)) {
+                    link.classList.add('active')
+                }
+            });
+        }
+
+    });
+});
